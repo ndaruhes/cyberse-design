78247a8dd5434d49c88c2cab9999b223356d95ab
Galvin Santoso <galvinsantoso22@gmail.com>
2022-06-27 01:16:08 +0700

    footer timeline

--- src/components/home/Agenda.vue
+++ src/components/home/Agenda.vue
@@ -1,10 +1,84 @@
 <template>
-    <div></div>
+  <div class="agenda">
+    <!-- agenda title -->
+    <h1 class="text-center text-main p-4">Explore Our Timeline</h1>
+    <!-- end agenda title -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 bg-card p-2 border-card overflow-hidden">
+          <div class="px-4 py-2 w-100">
+            <img src="@/assets/images/agenda/future.png" alt="future" class="img-fluid w-50 float-start px-4 mb-1" />
+            <h3 class="text-white text-justify">Perkenalan Metaverse</h3>
+            <p class="fs-6 text-justify text-white opacity-50">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel molestiae sapiente vitae! Voluptate laborum incidunt eius fugiat. Cum, excepturi pariatur?</p>
+          </div>
+        </div>
+        <div class="col-md-4 bg-card p-2 border-card overflow-hidden">
+          <div class="px-4 py-2 w-100">
+            <img src="@/assets/images/agenda/3.png" alt="future" class="img-fluid w-50 float-start px-4 mb-1" />
+            <h3 class="text-white text-justify">Keuntungan Metaverse</h3>
+            <p class="fs-6 text-justify text-white opacity-50">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel molestiae sapiente vitae! Voluptate laborum incidunt eius fugiat. Cum, excepturi pariatur?</p>
+          </div>
+        </div>
+        <div class="col-md-4 bg-card p-2 border-card overflow-hidden">
+          <div class="px-4 py-2 w-100">
+            <img src="@/assets/images/agenda/technology.png" alt="future" class="img-fluid w-50 float-start px-4 mb-1" />
+            <h3 class="text-white text-justify">Teknologi Metaverse</h3>
+            <p class="fs-6 text-justify text-white opacity-50">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel molestiae sapiente vitae! Voluptate laborum incidunt eius fugiat. Cum, excepturi pariatur?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.agenda {
+  width: 100%;
+  height: 100vh;
+  padding: 3rem;
+  .text-justify {
+    text-align: justify;
+  }
+  .bg-card {
+    border-radius: 1rem;
+  }
+  .border-card {
+    position: relative;
+    overflow: hidden;
+  }
+  .border-card::before {
+    content: '';
+    top: 25%;
+    position: absolute;
+    height: 40%;
+    width: 100%;
+    background-color: rgb(227, 53, 225);
+    filter: blur(10px);
+    border-radius: 1rem;
+    z-index: -2;
+    animation: 5s rotation infinite forwards;
+    animation-delay: -2s;
+  }
+  .border-card::after {
+    content: '';
+    position: absolute;
+    inset: 5px;
+    z-index: -1;
+    background-color: rgba(10, 11, 14, 1);
+    border-radius: 1rem;
+  }
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,43 @@
 <template>
-    <full-page ref="fullpage" :options="options" id="fullpage">
-        <!-- NAVBAR & HERO -->
-        <div class="section"><Navbar/><Hero/></div>
+  <full-page ref="fullpage" :options="options" id="fullpage">
+    <!-- NAVBAR & HERO -->
+    <div class="section"><Navbar /><Hero /></div>
 
-        <!-- AGENDA -->
-        <div class="section"><Agenda/></div>
+    <!-- AGENDA -->
+    <div class="section"><Agenda /></div>
 
-        <!-- INTRO -->
-        <div class="section"><Intro/></div>
+    <!-- INTRO -->
+    <div class="section"><Intro /></div>
 
-        <!-- KIND -->
-        <div class="section"><Kind/></div>
-    </full-page>
+    <!-- KIND -->
+    <div class="section"><Kind /></div>
+
+    <div class="section">
+      <Footer />
+    </div>
+  </full-page>
 </template>
 
 <script>
-import Navbar from "@/components/layouts/Navbar.vue";
-import Hero from "@/components/home/Hero.vue";
-import Agenda from "@/components/home/Agenda.vue";
-import Intro from "@/components/home/Intro.vue";
-import Kind from "@/components/home/Kind.vue";
+import Navbar from '@/components/layouts/Navbar.vue';
+import Hero from '@/components/home/Hero.vue';
+import Agenda from '@/components/home/Agenda.vue';
+import Intro from '@/components/home/Intro.vue';
+import Kind from '@/components/home/Kind.vue';
+import Footer from '@/components/layouts/Footer.vue';
 export default {
-    components: { Navbar, Hero, Agenda, Intro, Kind },
-    data() {
-        return {
-            options: {
-                licenseKey: null,
-                menu: "#menu",
-                navigation: true,
-                keyboardScrolling: true,
-                credits: { enabled: false },
-            },
-        };
-    },
-    methods: {},
+  components: { Navbar, Hero, Agenda, Intro, Kind, Footer },
+  data() {
+    return {
+      options: {
+        licenseKey: null,
+        menu: '#menu',
+        navigation: true,
+        keyboardScrolling: true,
+        credits: { enabled: false },
+      },
+    };
+  },
+  methods: {},
 };
 </script>
