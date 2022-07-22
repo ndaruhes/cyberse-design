795b5af5b9895eb0da754e2c4d35af4f072f7021
Galvin Santoso <galvinsantoso22@gmail.com>
2022-07-22 11:41:43 +0700

    masukan kata2

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,204 +1,243 @@
 <template>
-    <div class="community">
-        <div class="container">
-            <div class="header">
-                <h1 class="text-main">Cyberse Community</h1>
-                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit accusamus culpa fugiat doloribus</span>
-            </div>
+  <div class="community">
+    <div class="container">
+      <div class="header">
+        <h1 class="text-main">Cyberse Community</h1>
+        <span>Ayo bermain dan berdikusi tentang seputar dunia metaverse dalam ruang komunitas Cyberse</span>
+      </div>
 
-            <div class="socmed">
-                <div class="row">
-                    <div class="col-md-3 col-6 item">
-                        <a href="#" class="content-item discord">
-                            <div class="row align-items-center">
-                                <div class="col-4 icon">
-                                    <i class="uil uil-discord"></i>
-                                </div>
-                                <div class="col-8 text">
-                                    Discord <br> Community
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-3 col-6 item">
-                        <a href="#" class="content-item linkedin">
-                            <div class="row align-items-center">
-                                <div class="col-4 icon">
-                                    <i class="uil uil-linkedin"></i>
-                                </div>
-                                <div class="col-8 text">
-                                    LinkedIn <br> Community
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-3 col-6 item">
-                        <a href="#" class="content-item slack">
-                            <div class="row align-items-center">
-                                <div class="col-4 icon">
-                                    <i class="uil uil-slack"></i>
-                                </div>
-                                <div class="col-8 text">
-                                    Slack <br> Community
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-3 col-6 item">
-                        <a href="#" class="content-item twitter">
-                            <div class="row align-items-center">
-                                <div class="col-4 icon">
-                                    <i class="uil uil-twitter"></i>
-                                </div>
-                                <div class="col-8 text">
-                                    Twitter <br> Community
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+      <div class="socmed">
+        <div class="row">
+          <div class="col-md-3 col-6 item">
+            <a href="#" class="content-item discord">
+              <div class="row align-items-center">
+                <div class="col-4 icon">
+                  <i class="uil uil-discord"></i>
                 </div>
-            </div>
-
-            <div class="video">
-                <div class="row">
-                    <div class="col-md-6">
-                        <iframe src="https://www.youtube.com/embed/8J2M1H2GEhE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="iframe-left"></iframe>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="row">
-                            <div class="col-6">
-                                <iframe src="https://www.youtube.com/embed/bR8HtBM6F18" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="iframe-right"></iframe>
-                            </div>
-                            <div class="col-6">
-                                <iframe src="https://www.youtube.com/embed/KLOcj5qvOio" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="iframe-right"></iframe>
-                            </div>
-                            <div class="col-6">
-                                <iframe src="https://www.youtube.com/embed/HgIwgtN7xgU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="iframe-right"></iframe>
-                            </div>
-                            <div class="col-6">
-                                <iframe src="https://www.youtube.com/embed/rtLTZUaMSDQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="iframe-right"></iframe>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-8 text">
+                  Discord <br />
+                  Community
                 </div>
-            </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 col-6 item">
+            <a href="#" class="content-item linkedin">
+              <div class="row align-items-center">
+                <div class="col-4 icon">
+                  <i class="uil uil-linkedin"></i>
+                </div>
+                <div class="col-8 text">
+                  LinkedIn <br />
+                  Community
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 col-6 item">
+            <a href="#" class="content-item slack">
+              <div class="row align-items-center">
+                <div class="col-4 icon">
+                  <i class="uil uil-slack"></i>
+                </div>
+                <div class="col-8 text">
+                  Slack <br />
+                  Community
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3 col-6 item">
+            <a href="#" class="content-item twitter">
+              <div class="row align-items-center">
+                <div class="col-4 icon">
+                  <i class="uil uil-twitter"></i>
+                </div>
+                <div class="col-8 text">
+                  Twitter <br />
+                  Community
+                </div>
+              </div>
+            </a>
+          </div>
         </div>
+      </div>
+
+      <div class="video">
+        <div class="row">
+          <div class="col-md-6">
+            <iframe
+              src="https://www.youtube.com/embed/8J2M1H2GEhE"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+              class="iframe-left"
+            ></iframe>
+          </div>
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-6">
+                <iframe
+                  src="https://www.youtube.com/embed/bR8HtBM6F18"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                  class="iframe-right"
+                ></iframe>
+              </div>
+              <div class="col-6">
+                <iframe
+                  src="https://www.youtube.com/embed/KLOcj5qvOio"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                  class="iframe-right"
+                ></iframe>
+              </div>
+              <div class="col-6">
+                <iframe
+                  src="https://www.youtube.com/embed/HgIwgtN7xgU"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                  class="iframe-right"
+                ></iframe>
+              </div>
+              <div class="col-6">
+                <iframe
+                  src="https://www.youtube.com/embed/rtLTZUaMSDQ"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                  class="iframe-right"
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-@import "@/assets/sass/variables";
+@import '@/assets/sass/variables';
 .community {
-    .container {
-        height: 90vh;
-        // background: url("@/assets/images/community/2.png") no-repeat;
-        background-position: center;
-        background-size: cover;
-        border-radius: 20px;
-        position: relative;
-        padding: 20px;
-        .header {
-            color: $white;
-            margin-bottom: 40px;
-            text-align: center;
-            h1 {
-                text-transform: uppercase;
-            }
-            span {
-                font-size: 20px;
-                display: block;
-            }
-        }
-        .socmed {
-            .item {
-                .content-item {
-                    color: $white;
-                    display: block;
-                    text-decoration: none;
-                    border-radius: 10px;
-                    .icon {
-                        font-size: 70px;
-                        text-align: center;
-                    }
-                    .text {
-                        font-size: 25px;
-                    }
-                }
-
-                .discord {
-                    background: #485ef4;
-                }
-                .linkedin {
-                    background: #0077b5;
-                }
-                .slack {
-                    background: #491447;
-                }
-                .twitter {
-                    background: #5d9aea;
-                }
-            }
-        }
-        .video {
-            margin-top: 40px;
-            iframe {
-                border-radius: 10px;
-                width: 100%;
-            }
-            .iframe-left {
-                height: 360px;
-            }
-            .iframe-right {
-                margin-bottom: 20px;
-                height: 170px;
-            }
-        }
+  .container {
+    height: 90vh;
+    // background: url("@/assets/images/community/2.png") no-repeat;
+    background-position: center;
+    background-size: cover;
+    border-radius: 20px;
+    position: relative;
+    padding: 20px;
+    .header {
+      color: $white;
+      margin-bottom: 40px;
+      text-align: center;
+      h1 {
+        text-transform: uppercase;
+      }
+      span {
+        font-size: 20px;
+        display: block;
+      }
     }
+    .socmed {
+      .item {
+        .content-item {
+          color: $white;
+          display: block;
+          text-decoration: none;
+          border-radius: 10px;
+          .icon {
+            font-size: 70px;
+            text-align: center;
+          }
+          .text {
+            font-size: 25px;
+          }
+        }
+
+        .discord {
+          background: #485ef4;
+        }
+        .linkedin {
+          background: #0077b5;
+        }
+        .slack {
+          background: #491447;
+        }
+        .twitter {
+          background: #5d9aea;
+        }
+      }
+    }
+    .video {
+      margin-top: 40px;
+      iframe {
+        border-radius: 10px;
+        width: 100%;
+      }
+      .iframe-left {
+        height: 360px;
+      }
+      .iframe-right {
+        margin-bottom: 20px;
+        height: 170px;
+      }
+    }
+  }
 }
 @media screen and (max-width: 768px) {
-    .community {
-        padding: 0 0 220px 0;
-        .container {
-            .header {
-                margin-bottom: 40px;
-                h1 {
-                    font-size: 23px;
-                }
-                span {
-                    font-size: 15px;
-                }
-            }
-            .socmed {
-                .item {
-                    .content-item {
-                        margin-bottom: 20px;
-                        .icon {
-                            font-size: 45px;
-                            text-align: center;
-                            padding: 0 20px;
-                        }
-                        .text {
-                            font-size: 14px;
-                        }
-                    }
-                }
-            }
-            .video {
-                margin-top: 0;
-                iframe {
-                    border-radius: 10px;
-                    width: 100%;
-                }
-                .iframe-left {
-                    height: 180px;
-                    margin-bottom: 10px;
-                }
-                .iframe-right {
-                    margin-bottom: 10px;
-                    height: 85px;
-                }
-            }
+  .community {
+    padding: 0 0 220px 0;
+    .container {
+      .header {
+        margin-bottom: 40px;
+        h1 {
+          font-size: 23px;
         }
+        span {
+          font-size: 15px;
+        }
+      }
+      .socmed {
+        .item {
+          .content-item {
+            margin-bottom: 20px;
+            .icon {
+              font-size: 45px;
+              text-align: center;
+              padding: 0 20px;
+            }
+            .text {
+              font-size: 14px;
+            }
+          }
+        }
+      }
+      .video {
+        margin-top: 0;
+        iframe {
+          border-radius: 10px;
+          width: 100%;
+        }
+        .iframe-left {
+          height: 180px;
+          margin-bottom: 10px;
+        }
+        .iframe-right {
+          margin-bottom: 10px;
+          height: 85px;
+        }
+      }
     }
+  }
 }
 </style>
--- src/views/History.vue
+++ src/views/History.vue
@@ -1,98 +1,99 @@
 <template>
-    <div class="history">
-        <img src="@/assets/images/backgrounds/2.png" alt="background" class="background">
-        <div class="container">
-            <div class="back-section">
-                <button type="button" @click="checkHistory() ? $router.go(-1) : $router.push('/')" class="btn text-greenGradient">
-                    &laquo; Kembali
-                </button>
-            </div>
-            <div class="row align-items-center">
-                <div class="col-md-6 image">
-                    <img src="@/assets/images/image-shape/asset1.png" alt="image">
-                </div>
-                <div class="col-md-6 text">
-                    <h1 class="heading">
-                        Sejarah <span class="text-main fw-bold">Metaverse</span>
-                    </h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
-                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text</p>
-                </div>
-
-            </div>
+  <div class="history">
+    <img src="@/assets/images/backgrounds/2.png" alt="background" class="background" />
+    <div class="container">
+      <div class="back-section">
+        <button type="button" @click="checkHistory() ? $router.go(-1) : $router.push('/')" class="btn text-greenGradient">&laquo; Kembali</button>
+      </div>
+      <div class="row align-items-center">
+        <div class="col-md-6 image">
+          <img src="@/assets/images/image-shape/asset1.png" alt="image" />
         </div>
+        <div class="col-md-6 text">
+          <h1 class="heading">Sejarah <span class="text-main fw-bold">Metaverse</span></h1>
+          <p>
+            Konsep Metaverse diperkenalkan pertama kali dalam novel yang berjudul "Snow Crash" karya Neal Spehenson pada tahun 1992. Novel Snow Crash membahas berbagai manusias yang berinteraksi sebagai avatar dalam ruang virtual tiga
+            dimensi.
+          </p>
+          <p>
+            Film "Ready Player One" juga menjadi aspek yang berpengaruh dalam memperkenalkan dunia metaverse. Dalam film tersebut menampilkan berbagai manusia memnggunakan teknologi VR untuk masuk dan bermain ke dalam dunia yang disebut
+            OASIS. Konsep Metaverse diperkuat dengan Facebook mengganti nama menjadi "Meta" dan Mark Zuckerberg mengeluarkan platform bernama Mesh
+          </p>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import Mixins from "@/mixins";
+import Mixins from '@/mixins';
 export default {
-    mixins: [Mixins],
+  mixins: [Mixins],
 };
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/sass/variables.scss";
+@import '@/assets/sass/variables.scss';
 .history {
-    .background {
-        width: 90%;
-        position: absolute;
-        left: 5%;
-        bottom: 0;
-        z-index: 999;
-        opacity: 0.1;
+  .background {
+    width: 90%;
+    position: absolute;
+    left: 5%;
+    bottom: 0;
+    z-index: 999;
+    opacity: 0.1;
+  }
+  .back-section {
+    margin-bottom: 35px;
+    button {
+      z-index: 1000;
+      position: relative;
     }
-    .back-section {
-        margin-bottom: 35px;
-        button {
-            z-index: 1000;
-            position: relative;
-        }
+  }
+  .text {
+    .heading {
+      position: relative;
+      margin-bottom: 20px;
+      text-transform: uppercase;
+      color: $white;
     }
-    .text {
-        .heading {
-            position: relative;
-            margin-bottom: 20px;
-            text-transform: uppercase;
-            color: $white;
-        }
-        p {
-            color: $white;
-            font-size: 20px;
-        }
+    p {
+      color: $white;
+      font-size: 20px;
     }
-    .image {
-        text-align: center;
-        img {
-            width: 95%;
-            position: relative;
-            z-index: 1000;
-        }
+  }
+  .image {
+    text-align: center;
+    img {
+      width: 95%;
+      position: relative;
+      z-index: 1000;
     }
+  }
 }
 
 @media screen and (max-width: 768px) {
-    .history {
-        padding: 0 0 80px 0;
-        .back-section {
-            margin-bottom: 25px;
-        }
-
-        .text {
-            .heading {
-                margin-bottom: 20px;
-            }
-            p {
-                font-size: 15px;
-            }
-        }
-        .image {
-            margin-bottom: 20px;
-            img {
-                width: 90%;
-                margin-left: 5%;
-            }
-        }
+  .history {
+    padding: 0 0 80px 0;
+    .back-section {
+      margin-bottom: 25px;
     }
+
+    .text {
+      .heading {
+        margin-bottom: 20px;
+      }
+      p {
+        font-size: 15px;
+      }
+    }
+    .image {
+      margin-bottom: 20px;
+      img {
+        width: 90%;
+        margin-left: 5%;
+      }
+    }
+  }
 }
 </style>
